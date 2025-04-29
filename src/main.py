5576c6ae5cd39b29e983488de9ad886c97cf8681
Illia Siutkin <illiasiutkin07@gmail.com>
2025-04-29 09:05:14 -0500

    Pipe chatgpt response to a pdf page
    Co-authored-by: partik103 <partik103@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -13,14 +13,15 @@ gemini_api_key = data.get("gemini-api-key")
 client = genai.Client(api_key=gemini_api_key)
 
 # Send prompt
-prompt = "Why is the sky blue?"
+prompt = "Should the math folder be red or blue"
 response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+print(response.text)
 
 # Create PDF
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font("helvetica", size=12)
-pdf.cell(text="Test")
+pdf.set_font("courier", size=12)
+pdf.multi_cell(0, 10, txt=response.text)
 pdf.output("test.pdf")
 
 # # TODO: create a GUI with tk
